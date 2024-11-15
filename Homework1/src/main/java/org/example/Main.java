f25bfd27b399ca8c35c932fcf63bd75e8b98e942
OsmanovRuslan <ruslan.osmanov.2023@mail.ru>
2024-11-15 21:46:02 +0300

    Added Homework1

---
+++ Homework1/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Child firstChild = new Child();
+        Child secondChild = new Child("Ivan");
+    }
+}
---
+++ Homework1/src/main/java/org/example/Parent.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public abstract class Parent {
+
+    private String Name;
+
+    static {
+        System.out.println("Parent:static 1");
+    }
+
+    {
+        System.out.println("Parent:instance 1");
+    }
+
+    static {
+        System.out.println("Parent:static 2");
+    }
+
+    public Parent() {
+        System.out.println("Parent:constructor");
+    }
+
+    {
+        System.out.println("Parent:instance 2");
+    }
+
+    public Parent(String Name) {
+        System.out.println("Parent:name-constructor");
+    }
+
+}
