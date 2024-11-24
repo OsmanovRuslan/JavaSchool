b3b0ecc84a4dd5d11ab5e6f2c6e4b6e25440756b
OsmanovRuslan <ruslan.osmanov.2023@mail.ru>
2024-11-24 22:07:08 +0300

    Corrected

--- Homework1/src/main/java/org/example/Child.java
+++ Homework1/src/main/java/org/example/Child.java
@@ -6,16 +6,12 @@ public class Child extends Parent{
         System.out.println("Child:static 1");
     }
 
-    static {
-        System.out.println("Child:static 2");
-    }
-
     {
         System.out.println("Child:instance 1");
     }
 
-    {
-        System.out.println("Child:instance 2");
+    static {
+        System.out.println("Child:static 2");
     }
 
     public Child() {
@@ -25,6 +21,10 @@ public class Child extends Parent{
     public Child(String Name) {
         super(Name);
         System.out.println("Child:name-constructor");
+    }
+
+    {
+        System.out.println("Child:instance 2");
     }
 
 }
